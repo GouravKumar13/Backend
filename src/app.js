ee8ad5b8ab132b1d03be42252f5d2f9a05bfc20d
GouravKumar13 <gouravkumarat@gmail.com>
2024-02-19 12:56:58 +0530

    controllers

--- src/app.js
+++ src/app.js
@@ -14,8 +14,13 @@ app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 // this helps us to store img or pdf's  or public assets on the server
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+//router import file segregation routes
+import UserRouter from './routes/user.routes.js';
+
+app.use("/api/v1/users", UserRouter)
+
 
 
 export { app }
